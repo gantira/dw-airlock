a862026328a693e9c7d918a610d785120b53faa3
Angga Gantira <angga.gantira@gmail.com>
2020-03-21 06:40:15 +0700

    Mengenal & Mengimplementasikan Laravel Airlock

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//ROUTING YANG INGIN DIPROTECT DENGAN AIRLOCK, MAKA HARUS MENGGUNAKAN MIDDLEWARE DARI AUTH:AIRLOCK
+Route::group(['middleware' => 'auth:airlock'], function () {
+    //SEHINGGA SEMUA ROUTING YANG ADA DI DALAMNYA HARUS MENGIRIMKAN TOKEN
+    Route::get('/users', 'UserController@index');
+    Route::post('/users', 'UserController@store');
+    Route::get('/users/tokens', 'UserController@getAllUserToken');
+    Route::get('/users/delete', 'UserController@revokeToken');
+});
+
+Route::post('/login', 'UserController@login');
